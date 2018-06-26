42ba3d503dda6e07e21cf110c0a6dacbd7854d75
Razvan Oprea <razvan.oprea96@yahoo.com>
2018-06-26 20:47:56 +0300

    upd (scos licenta)

--- Take-Me-To-Dinner/WebApp/Pages/SearchPage.aspx
+++ Take-Me-To-Dinner/WebApp/Pages/SearchPage.aspx
@@ -1,8 +1,8 @@
-﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApp._Default" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SearchPage.aspx.cs" Inherits="WebApp.Pages.SearchPage" MasterPageFile="~/Site.Master" %>
 
-<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server"  >
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server" >
     
-    <link rel="stylesheet" href="Content/SearchPage.css?version=8" type="text/css" />
+    <link rel="stylesheet" href="Content/SearchPage.css" type="text/css" />
 
     <div class="container">
         <div class="row">
---
+++ Take-Me-To-Dinner/WebApp/Pages/SearchPage.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.Pages
+{
+    public partial class SearchPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ddlCities_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Response.Redirect("Pages/ShowPlaces.aspx?cityId=" + ddlCities.SelectedItem.Value);
+        }
+
+        protected void ddlCities_Init(object sender, EventArgs e)
+        {
+            DropDownList ddl = (DropDownList)sender;
+            ddl.Items[0].Attributes.Add("disabled", "disabled");
+        }
+    }
+}
